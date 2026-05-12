6a110fecd23c4a10b9e8ec271ff11e7be40b0976
lukylinek <lukas.kumpan@unob.cz>
2026-05-12 10:37:09 +0200

    Odstraneni delete a create

--- packages/projekt_lukas/src/ProjectGQLModel/Mutations/InteractiveMutations.jsx
+++ packages/projekt_lukas/src/ProjectGQLModel/Mutations/InteractiveMutations.jsx
@@ -1,8 +1,6 @@
 import { CardCapsule, VectorItemsURI } from "../Components"
-import { CreateButton, CreateLink } from "./Create"
 import { UpdateButton, UpdateLink } from "./Update"
 import { ProxyLink } from "../../../../_template/src/Base/Components/ProxyLink"
-import { DeleteButton } from "./Delete"
 
 export const PageLink = ({ children, preserveHash = true, preserveSearch = true, ...props }) => {
     return (
@@ -20,11 +18,17 @@ export const PageLink = ({ children, preserveHash = true, preserveSearch = true,
 export const InteractiveMutations = ({ item }) => {
     return (
         <CardCapsule item={item} title="Nástroje">
-            <PageLink className="btn btn-outline-success">Stránka</PageLink>
-            <UpdateLink className="btn btn-outline-success" item={item}>Upravit</UpdateLink>
-            <UpdateButton className="btn btn-outline-success" item={item}>Upravit Dialog</UpdateButton>
-            <CreateButton className="btn btn-outline-success" rbacitem={{}}>Vytvořit nový</CreateButton>
-            <DeleteButton className="btn btn-outline-danger" item={item}>Odstranit</DeleteButton>
+            <PageLink className="btn btn-outline-success">
+                Stránka
+            </PageLink>
+
+            <UpdateLink className="btn btn-outline-success" item={item}>
+                Upravit
+            </UpdateLink>
+
+            <UpdateButton className="btn btn-outline-success" item={item}>
+                Upravit Dialog
+            </UpdateButton>
         </CardCapsule>
     )
 }
